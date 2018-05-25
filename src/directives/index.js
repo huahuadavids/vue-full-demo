bbbe8dc4ab95d2119615a7506f8af552f24ce86c
zhangyuhua <1457896625@qq.com>
2018-05-25 10:05:38 +0800

    add http,directive,mixin

---
+++ src/directives/index.js
@@ -0,0 +1,22 @@
+/**
+ * @author huahuadavids
+ * @description all the directives
+ *
+ */
+const directives = {
+  /**
+   *@description
+   */
+  'my-directive' :{
+    bind(){},
+    inserted() {},
+    update() {},
+    componentUpdated() {},
+    unbind() {}
+  }
+
+
+};
+
+
+export default directives;
